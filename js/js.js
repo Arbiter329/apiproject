add203cc79c04897c9d651c37085c2967bf94bcd
Henry Ammons <Henry Ammons>
2017-02-09 14:12:09 -0500

    API Setup

--- js/js.js
+++ js/js.js
@@ -1,1 +1,17 @@
-alert("OH GOD HELP ME IM ON FIRE!?");
+$(document).ready(function () {
+    var gameList = document.getElementById("gameList"),
+        gameButton = document.getElementById("gameButton"),
+        searchGame = document.getElementById("searchGame"),
+        gamesUri = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10&offset=0&order=release_dates.date%3Adesc&search=";
+
+    gameButton.addEventListener("click", function () {
+        $.ajax({
+            method: "GET",
+            url: gamesUri + searchGame.value,
+            headers: {'X-Mashape-Key': 'rEBY1HLvaFmshm5i2THU9cOrtMYNp1PiP4yjsnbH2afPajCGqU', 'Accept': 'application/json'}
+        })
+            .done(function (msg) {
+                alert("Data Saved: " + msg);
+            });
+    });
+});
